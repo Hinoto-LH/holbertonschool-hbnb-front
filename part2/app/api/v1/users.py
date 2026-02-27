559fb53280ea2ad9e62d36be150411354d5e3160
Hinoto-LH <masseron.a.pro@gmail.com>
2026-02-27 16:11:13 +0100

    Dev api get user (#7)
    * Add new fonction: get_all_user
    
    * fix code: get_all_user - add new attribut
    
    * Add endpoint reviews

--- part2/app/api/v1/users.py
+++ part2/app/api/v1/users.py
@@ -5,8 +5,10 @@ api = Namespace('users', description='User operations')
 
 # Define the user model for input validation and documentation
 user_model = api.model('User', {
-    'first_name': fields.String(required=True, description='First name of the user'),
-    'last_name': fields.String(required=True, description='Last name of the user'),
+    'first_name': fields.String(required=True,
+                                description='First name of the user'),
+    'last_name': fields.String(required=True,
+                               description='Last name of the user'),
     'email': fields.String(required=True, description='Email of the user')
 })
 
@@ -39,7 +41,8 @@ class UserList(Resource):
         return [{
                 'id': a.id,
                 'first_name': a.first_name,
-                'last_name': a.last_name
+                'last_name': a.last_name,
+                'email': a.email
                 } for a in users], 200
 
 
